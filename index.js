7770cd7a8bb3fc7ca64d48f635cad7bbb30146da
yasemincidem <yasemincidem@gmail.com>
2017-06-20 10:13:05 +0300

    index.js is changed

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+import {NativeModules,ToastAndroid,Platform} from 'react-native';
 
-import { NativeModules } from 'react-native';
+var RCTToastAndroid = Platform.OS === 'android' ? ToastAndroid : NativeModules.RNToastNative;
 
-const { RNToastNative } = NativeModules;
+var ToastNative = {
+    // Toast duration constants
+    SHORT: RCTToastAndroid.SHORT,
+    LONG: RCTToastAndroid.LONG,
 
-export default RNToastNative;
+    // Toast gravity constants
+    TOP: RCTToastAndroid.TOP,
+    BOTTOM: RCTToastAndroid.BOTTOM,
+    CENTER: RCTToastAndroid.CENTER,
+
+    show: function (
+        message,
+        duration,
+        customStyle
+    ) {
+        RCTToastAndroid.show(message, duration === undefined ? this.SHORT : duration,customStyle);
+    },
+
+    showWithGravity: function (
+        message,
+        duration,
+        gravity,
+        customStyle
+    ) {
+        RCTToastAndroid.showWithGravity(message, duration === undefined ? this.SHORT : duration, customStyle, gravity);
+    },
+};
+
+export default ToastNative;
