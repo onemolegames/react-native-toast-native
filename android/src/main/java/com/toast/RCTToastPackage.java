83f67e21cc5aa9d7d4862c35369b56264e9476c7
semirturgay <semir.turgay@gmail.com>
2017-06-27 14:14:57 +0300

    Android version is updated

--- android/src/main/java/com/toast/RCTToastPackage.java
+++ android/src/main/java/com/toast/RCTToastPackage.java
@@ -1,28 +1,30 @@
-
-package com.reactlibrary;
+package com.toast;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
-public class RNToastNativePackage implements ReactPackage {
+
+public class RCTToastPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNToastNativeModule(reactContext));
+        return Arrays.<NativeModule>asList(
+                new Toast(reactContext)
+        );
     }
 
     @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Arrays.<ViewManager>asList();
     }
 }
