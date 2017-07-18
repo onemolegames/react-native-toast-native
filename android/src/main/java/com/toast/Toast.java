cb75681e88089bf5a4180dedf96e1ed85d155412
yasemincidem <yasemincidem@gmail.com>
2017-07-19 00:31:01 +0300

    android version is changed

--- android/src/main/java/com/toast/Toast.java
+++ android/src/main/java/com/toast/Toast.java
@@ -2,6 +2,8 @@ package com.toast;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
 import android.text.Layout;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -36,11 +38,20 @@ public class Toast extends ReactContextBaseJavaModule implements LifecycleEventL
         if (this.isPaused) {
             return;
         }
-        final String backgroundColor = styles.getString("backgroundColor");
-        final String color = styles.getString("color");
-        final int width = styles.getInt("width");
-        final int height = styles.getInt("height");
-        final int fontSize = styles.getInt("fontSize");
+        final String backgroundColor = styles.hasKey("backgroundColor") ? styles.getString("backgroundColor") : "#000000";
+        final String color = styles.hasKey("color") ? styles.getString("color") : "#ffffff";
+        final int width = styles.hasKey("width") ? styles.getInt("width") : 50;
+        final int height = styles.hasKey("height") ? styles.getInt("height") : 50;
+        final int paddingLeft = styles.hasKey("paddingLeft") ? styles.getInt("paddingLeft") : 10;
+        final int paddingRight = styles.hasKey("paddingRight") ? styles.getInt("paddingRight") : 10;
+        final int paddingTop = styles.hasKey("paddingTop") ? styles.getInt("paddingTop") : 5;
+        final int paddingBottom = styles.hasKey("paddingBottom") ? styles.getInt("paddingTop") : 5;
+        final int fontSize = styles.hasKey("fontSize") ? styles.getInt("fontSize") : 12;
+        final int lineHeight = styles.hasKey("lineHeight") ? styles.getInt("lineHeight") : 10;
+        final int cornerRadius = styles.hasKey("cornerRadius") ? styles.getInt("cornerRadius") : 10;
+        final int lines = styles.hasKey("lines") ? styles.getInt("lines") : 3;
+        final float letterSpacing = styles.hasKey("letterSpacing") ? (float)styles.getDouble("letterSpacing") : 0;
+
         UiThreadUtil.runOnUiThread(new Runnable() {
             public void run() {
                 android.widget.Toast toast = android.widget.Toast.makeText(
@@ -49,17 +60,27 @@ public class Toast extends ReactContextBaseJavaModule implements LifecycleEventL
                         "short".equals(duration) ? android.widget.Toast.LENGTH_SHORT : android.widget.Toast.LENGTH_LONG);
                 View view = toast.getView();
                 TextView text = (TextView) view.findViewById(android.R.id.message);
-                view.setMinimumWidth(width);
-                view.setMinimumHeight(height);
-                text.setTextColor(Color.parseColor(color));
-                text.setTextSize(fontSize);
+                GradientDrawable gd = new GradientDrawable();
                 view.setBackgroundColor(Color.parseColor(backgroundColor));
+                view.setPadding(paddingLeft,paddingTop,paddingRight,paddingBottom);
+                view.setMinimumHeight(height);
+                view.setMinimumWidth(width);
+                text.setTextSize(fontSize);
+                text.setLines(lines);
+                text.setMaxLines(lines);
+                text.setHeight(lineHeight);
+                text.setLetterSpacing(letterSpacing);
+                text.setTypeface(Typeface.SANS_SERIF);
+                gd.setStroke(2, Color.parseColor(backgroundColor));
+                gd.setColor(Color.parseColor(backgroundColor));
+                gd.setCornerRadius(cornerRadius);
+                view.setBackground(gd);
                 if ("top".equals(position)) {
                     toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 20);
                 } else if ("bottom".equals(position)) {
                     toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 20);
                 } else if ("center".equals(position)) {
-                    toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, 0);
+                    toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, 20);
                 } else {
                     FLog.e("RCTToast", "invalid position. valid options are 'top', 'center' and 'bottom'");
                     return;
