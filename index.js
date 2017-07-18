ad677c65da0877d32888dc6881148a91fe86240f
yasemincidem <yasemincidem@gmail.com>
2017-07-19 00:59:52 +0300

    android version is changed v3

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 import {NativeModules,ToastAndroid,Platform} from 'react-native';
 
-var RCTToastAndroid = Platform.OS === 'android' ? NativeModules.Toast : NativeModules.RNToastNative;
+if(Platform.OS === "android"){
+    var ToastNative = {
+        show: function (
+            message,
+            duration,
+            position,
+            styles
+        ) {
+            NativeModules.Toast.show(message, duration,position,styles);
+        }
+    };
+}else{
+    var ToastNative = {
+        // Toast duration constants
+        SHORT: RCTToastAndroid.SHORT,
+        LONG: RCTToastAndroid.LONG,
 
-var ToastNative = {
-    // Toast duration constants
-    // SHORT: RCTToastAndroid.SHORT,
-    // LONG: RCTToastAndroid.LONG,
-    //
-    // // Toast gravity constants
-    // TOP: RCTToastAndroid.TOP,
-    // BOTTOM: RCTToastAndroid.BOTTOM,
-    // CENTER: RCTToastAndroid.CENTER,
+        // Toast gravity constants
+        TOP: RCTToastAndroid.TOP,
+        BOTTOM: RCTToastAndroid.BOTTOM,
+        CENTER: RCTToastAndroid.CENTER,
 
-    show: function (
-        message,
-        duration,
-        position,
-        styles
-    ) {
-        RCTToastAndroid.show(message, duration,position,styles);
-    }
-};
+        show: function (
+            message,
+            duration,
+            customStyle
+        ) {
+            NativeModules.RNToastNative.show(message, duration === undefined ? this.SHORT : duration,customStyle);
+        },
+
+        showWithGravity: function (
+            message,
+            duration,
+            gravity,
+            customStyle
+        ) {
+            NativeModules.RNToastNative.showWithGravity(message, duration === undefined ? this.SHORT : duration, customStyle, gravity);
+        },
+    };
+}
 
 export default ToastNative;
