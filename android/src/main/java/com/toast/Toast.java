0de1e648235c3fc656926d512a9696b47e74a26c
yasemincidem <yasemincidem@gmail.com>
2017-07-18 17:32:58 +0300

    android version is corrected

--- android/src/main/java/com/toast/Toast.java
+++ android/src/main/java/com/toast/Toast.java
@@ -1,89 +1,103 @@
 package com.toast;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.text.Layout;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.facebook.common.logging.FLog;
 import com.facebook.react.bridge.*;
 
+import java.util.Objects;
+
 public class Toast extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
-  private android.widget.Toast mostRecentToast;
+    private android.widget.Toast mostRecentToast;
 
-  // note that webView.isPaused() is not Xwalk compatible, so tracking it poor-man style
-  private boolean isPaused;
+    // note that webView.isPaused() is not Xwalk compatible, so tracking it poor-man style
+    private boolean isPaused;
 
-  public Toast(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
-
-  @Override
-  public String getName() {
-    return "RCTToast";
-  }
-
-  @ReactMethod
-  public void show(ReadableMap options) throws Exception {
-    if (this.isPaused) {
-      return;
+    public Toast(ReactApplicationContext reactContext) {
+        super(reactContext);
     }
 
+    @Override
+    public String getName() {
+        return "RCTToast";
+    }
 
-    final String message = options.getString("message");
-    final String duration = options.getString("duration");
-    final String position = options.getString("position");
-    final int addPixelsY = options.hasKey("addPixelsY") ? options.getInt("addPixelsY") : 0;
+    @ReactMethod
+    public void show(final String message, final int duration, final String position, ReadableMap styles) throws Exception {
 
-    UiThreadUtil.runOnUiThread(new Runnable() {
-      public void run() {
-        android.widget.Toast toast = android.widget.Toast.makeText(
-                getReactApplicationContext(),
-                message,
-                "short".equals(duration) ? android.widget.Toast.LENGTH_SHORT : android.widget.Toast.LENGTH_LONG);
-
-        if ("top".equals(position)) {
-          toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 20 + addPixelsY);
-        } else if ("bottom".equals(position)) {
-          toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 20 - addPixelsY);
-        } else if ("center".equals(position)) {
-          toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, addPixelsY);
-        } else {
-          FLog.e("RCTToast", "invalid position. valid options are 'top', 'center' and 'bottom'");
-          return;
+        if (this.isPaused) {
+            return;
         }
-
-        toast.show();
-        mostRecentToast = toast;
-      }
-    });
-  }
-
-  @ReactMethod
-  public void hide() throws Exception {
-    if (mostRecentToast != null) {
-      mostRecentToast.cancel();
+        final String backgroundColor = styles.getString("backgroundColor");
+        final String color = styles.getString("color");
+        final int width = styles.getInt("width");
+        final int height = styles.getInt("height");
+        final int fontSize = styles.getInt("fontSize");
+        UiThreadUtil.runOnUiThread(new Runnable() {
+            public void run() {
+                android.widget.Toast toast = android.widget.Toast.makeText(
+                        getReactApplicationContext(),
+                        message,
+                        "short".equals(duration) ? android.widget.Toast.LENGTH_SHORT : android.widget.Toast.LENGTH_LONG);
+                View view = toast.getView();
+                TextView text = (TextView) view.findViewById(android.R.id.message);
+                view.setMinimumWidth(width);
+                view.setMinimumHeight(height);
+                text.setTextColor(Color.parseColor(color));
+                text.setTextSize(fontSize);
+                view.setBackgroundColor(Color.parseColor(backgroundColor));
+                if ("top".equals(position)) {
+                    toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 20);
+                } else if ("bottom".equals(position)) {
+                    toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 20);
+                } else if ("center".equals(position)) {
+                    toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, 0);
+                } else {
+                    FLog.e("RCTToast", "invalid position. valid options are 'top', 'center' and 'bottom'");
+                    return;
+                }
+                toast.setView(view);
+                toast.show();
+                mostRecentToast = toast;
+            }
+        });
     }
-  }
 
-  @Override
-  public void initialize() {
-    getReactApplicationContext().addLifecycleEventListener(this);
-  }
-
-  @Override
-  public void onHostPause() {
-    if (mostRecentToast != null) {
-      mostRecentToast.cancel();
+    @ReactMethod
+    public void hide() throws Exception {
+        if (mostRecentToast != null) {
+            mostRecentToast.cancel();
+        }
     }
-    this.isPaused = true;
-  }
 
-  @Override
-  public void onHostResume() {
-    this.isPaused = false;
-  }
+    @Override
+    public void initialize() {
+        getReactApplicationContext().addLifecycleEventListener(this);
+    }
 
-  @Override
-  public void onHostDestroy() {
-    this.isPaused = true;
-  }
+    @Override
+    public void onHostPause() {
+        if (mostRecentToast != null) {
+            mostRecentToast.cancel();
+        }
+        this.isPaused = true;
+    }
+
+    @Override
+    public void onHostResume() {
+        this.isPaused = false;
+    }
+
+    @Override
+    public void onHostDestroy() {
+        this.isPaused = true;
+    }
 }
--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 import {NativeModules,ToastAndroid,Platform} from 'react-native';
 
-var RCTToastAndroid = Platform.OS === 'android' ? ToastAndroid : NativeModules.RNToastNative;
+var RCTToastAndroid = Platform.OS === 'android' ? NativeModules.Toast : NativeModules.RNToastNative;
 
 var ToastNative = {
     // Toast duration constants
-    SHORT: RCTToastAndroid.SHORT,
-    LONG: RCTToastAndroid.LONG,
-
-    // Toast gravity constants
-    TOP: RCTToastAndroid.TOP,
-    BOTTOM: RCTToastAndroid.BOTTOM,
-    CENTER: RCTToastAndroid.CENTER,
+    // SHORT: RCTToastAndroid.SHORT,
+    // LONG: RCTToastAndroid.LONG,
+    //
+    // // Toast gravity constants
+    // TOP: RCTToastAndroid.TOP,
+    // BOTTOM: RCTToastAndroid.BOTTOM,
+    // CENTER: RCTToastAndroid.CENTER,
 
     show: function (
         message,
         duration,
-        customStyle
+        position,
+        styles
     ) {
-        RCTToastAndroid.show(message, duration === undefined ? this.SHORT : duration,customStyle);
-    },
-
-    showWithGravity: function (
-        message,
-        duration,
-        gravity,
-        customStyle
-    ) {
-        RCTToastAndroid.showWithGravity(message, duration === undefined ? this.SHORT : duration, customStyle, gravity);
-    },
+        RCTToastAndroid.show(message, duration,position,styles);
+    }
 };
 
 export default ToastNative;
