75dd731950b9e6abb6092ecc3ac39a8d7ab649ee
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2017-09-05 22:59:08 +0300

    RN 0.47 fix

--- android/src/main/java/com/toast/RCTToastPackage.java
+++ android/src/main/java/com/toast/RCTToastPackage.java
@@ -19,11 +19,6 @@ public class RCTToastPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
     }
